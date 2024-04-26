5893fc0ef3bbef4e60310d616e62db7efc0d18a0
Jaden-Reklaw <jwalker117@gmail.com>
2024-04-26 17:45:58 -0500

    AWS API Gateway and Lambda

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,16 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { join } from 'path';
 export class LambdaStack extends Stack {
 
+    public readonly spacesLambdaIntegration: LambdaIntegration;
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        new LambdaFunction(this, 'HelloLambda', {
+        const spacesLambda = new LambdaFunction(this, 'SpacesLambda', {
             runtime: Runtime.NODEJS_20_X,
             handler: 'hello.main',
             code: Code.fromAsset(join(__dirname, '..', '..', 'services'))
-        })
+        });
+
+        this.spacesLambdaIntegration = new LambdaIntegration(spacesLambda);
     }
 }
--- src/services/hello.js
+++ src/services/hello.js
@@ -1,8 +1,6 @@
-
 exports.main = async function(event, context) {
     return {
         statusCode: 200,
-        headers: "application/json",
-        body: JSON.stringify({message: "Hello from Spaces Lambda!"})
+        body: JSON.stringify('Hello from lambda!')
     }
 }
